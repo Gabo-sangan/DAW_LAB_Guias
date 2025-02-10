863eebd13508f78b921f321cc3330666b752e38e
Gabo-sangan <gabrielmario1664@gmail.com>
2025-02-09 22:33:08 -0600

    Add files via upload

---
+++ hr242882_Guia02/ejemplo2/js/rectangulo.js
@@ -0,0 +1,7 @@
+var base = 0
+var altura = 0
+base = prompt('Introduzca la base del rectángulo:','');
+altura = prompt('Introduzca la altura del rectángulo:','');
+var area;
+area = base*altura;
+document.write("<header><h1>El área del triangulo es : " + area + "</h1><hr/><br/></header>")
---
+++ hr242882_Guia02/ejemplo2/js/triangulo.js
@@ -0,0 +1,7 @@
+var base = 0
+var altura = 0
+base = prompt('Introduzca la base del triángulo:','');
+altura = prompt('Introduzca la altura del triángulo:','');
+var area;
+area = (base*altura)/2;
+document.write("<header><h1>El área del triangulo es : " + area + "</h1><hr/><br/></header>")
